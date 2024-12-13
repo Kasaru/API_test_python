79f69f1887955ec3de00e2bc1382de8bb499c448
Artem <arte.kats@gmail.com>
2024-12-13 19:47:08 +0300

    1.Отправить запрос для получения всех категорий - create_categories_list добавлено 2.Получить 1 шутку по каждой из категорий (16 шт) - всего 16 шуток - test_create_joke_category_positive запускается для каждой категории из списка полученного в create_categories_list

---
+++ Chuck_api/GET/Create_joke.py
@@ -0,0 +1,46 @@
+import requests
+from unicodedata import category
+
+
+class TestCreateJoke:
+
+    url = 'https://api.chucknorris.io/jokes/random'
+
+    def create_categories_list(self):
+        url = 'https://api.chucknorris.io/jokes/'
+        result = requests.get(url + 'categories')
+        categories = result.json()
+        return categories
+
+    def test_create_joke_category_positive(self, category, expected_status_code):
+        path_joke_category = f"?category={category}"
+        url_joke_category = self.url + path_joke_category
+        print(url_joke_category)
+
+        result = requests.get(url_joke_category)
+        print(result.json())
+
+        print(f'Статус-код: {result.status_code}')
+        assert result.status_code == expected_status_code, 'ОШИБКА, Статус-код не совпадают'
+        print('Статус-код корректен')
+
+        check_joke = result.json()
+        joke_value = check_joke.get("value")
+        print(joke_value)
+
+        joke_category = check_joke.get("categories")
+        print(joke_category)
+        assert joke_category[0] == category, 'ОШИБКА, Статус-код не совпадает'
+        print('Категория корректна')
+
+        assert_word = 'Chuck'
+        assert assert_word in joke_value, 'ОШИБКА, Проверочное слово отсутствует'
+        print('Проверочное слово присутствует')
+        print("Тест прошел успешно")
+
+
+
+start = TestCreateJoke()
+category_list = start.create_categories_list()
+for category in category_list:
+    start.test_create_joke_category_positive(category, 200)
